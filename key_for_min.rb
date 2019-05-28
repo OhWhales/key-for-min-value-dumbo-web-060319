b2199d370f4df9ef05dbdcf4171930e8fca8212b
impartial-flow-3347 <brandon.lau86@gmail.com>
2019-05-28 03:33:42 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,9 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  answer= 100000
   name_hash.each do |key, val|
     
+    if val < answer 
+      answer = val 
 end
